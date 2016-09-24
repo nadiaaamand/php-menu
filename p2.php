8a42a5a2f9ae29a777685d859e4771c09c4df318
Nadia Aamand <nadiaaamand@gmail.com>
2016-09-24 15:24:44 +0200

    attached a ccs file
    started css

--- p2.php
+++ p2.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="UTF-8">
 <title>Sevices</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-
 <body>
 <?php include 'menu.php'; ?>
 <h1>Services</h1>
